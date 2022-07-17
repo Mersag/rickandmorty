161d2720b45fcc346b8ed9edf3acb94071a836ee
Meral Sager <mersa22@gmx.de>
2022-07-17 17:40:40 +0200

    Add Fetch

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+console.clear();
 console.log('hello world!');
+// URL als variable anlegen 
+const url = "https://rickandmortyapi.com/api/location";
+// erstellen eine fetch und holen uns sämtliche Locations Daten  
+const locations = fetch(url)
+    .then((jsonObject) => jsonObject.json())
+    .then((javascriptObject) => {
+        console.log(javascriptObject.results)
+        return javascriptObject.results;
+    }).catch(error => console.error(error.message));
+console.log(locations);
