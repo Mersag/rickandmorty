388e2ad7d94303dcec9bc17797ddc57d3241cae5
Stefan Helms <mail@estefan.de>
2022-07-18 09:27:08 +0200

    Filter planets

--- index.js
+++ index.js
@@ -2,11 +2,43 @@ console.clear();
 console.log('hello world!');
 // URL als variable anlegen 
 const url = "https://rickandmortyapi.com/api/location";
+const locations = [];
 // erstellen eine fetch und holen uns sämtliche Locations Daten  
-const locations = fetch(url)
+fetch(url)
     .then((jsonObject) => jsonObject.json())
     .then((javascriptObject) => {
-        console.log(javascriptObject.results)
-        return javascriptObject.results;
+        //console.log(javascriptObject.results)
+        displayLocations(javascriptObject.results);
     }).catch(error => console.error(error.message));
-console.log(locations);
+
+
+function displayLocations(locations){
+    console.log(locations.length);
+    // Filter-Funktion aufrufen
+    filterLocations(locations);
+}
+// Filter-Funktion definieren
+function filterLocations(locations){
+    const allPlanets = locations.filter((location) => {
+        if (location.type==="Planet" && location.dimension==="unknown") {
+         
+            return true;
+        
+        }
+        return false;
+      });
+      
+      //console.log(allPlanets);
+      showPlanets(allPlanets);
+}
+function showPlanets(planets){
+    planets.forEach((planet)=>{
+          writeHtml(planet.name);  
+    }  
+    )
+}
+
+function writeHtml(){
+    const planets = document.querySelector('[data-js="planets"]');
+    
+}
